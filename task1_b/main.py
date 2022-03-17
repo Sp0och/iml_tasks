b361870ea0e7bd97abcc4d46a91212333a9e11c0
Jonny-Air <jojobecker99@gmail.com>
2022-03-17 12:07:03 +0100

    working task1b

---
+++ task1_b/main.py
@@ -0,0 +1,23 @@
+from numpy import dtype
+import pandas as pd
+import numpy as np
+from numpy.linalg import lstsq
+
+# split train dataset into 10 equal parts
+test_file = "./train.csv"
+
+data = pd.read_csv(test_file, float_precision="round_trip")
+data = data.to_numpy()
+y = np.reshape(data[:,1], (-1, 1))
+x = data[:,2:]
+
+x2 = np.square(x)
+exponential = np.exp(x)
+cos = np.cos(x)
+
+# least squares fit
+A = np.concatenate([x, x2, exponential, cos, np.ones_like(y)], axis=1)
+sol = np.linalg.lstsq(A, y, rcond=None)[0]
+
+print(sol)
+np.savetxt("results.csv", sol, delimiter="\n", fmt='%4.12f')
