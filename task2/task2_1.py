c68573c2bb392a642ee506560d198a9f6ff8bf96
Jonathan Becker <jonbecke@student.ethz.ch>
2022-04-21 15:46:32 +0200

    working fit with average 75% accuracy

--- task2/task2_1.py
+++ task2/task2_1.py
@@ -1,13 +1,15 @@
 import numpy as np
 from sklearn import svm
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.calibration import CalibratedClassifierCV
 import matplotlib.pyplot as plt
 import pandas as pd
 from alive_progress import alive_bar
-from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
 
-feature_df = pd.read_csv("normalized_train_features.csv", )
+feature_df = pd.read_csv("output_data/train_features_processed.csv", )
 label_df = pd.read_csv("input_data/train_labels.csv")
 feature_df.sort_values(by='pid')
 label_df.sort_values(by=['pid'])
@@ -46,14 +48,25 @@ with alive_bar(len(test_label_names)) as bar:
     print(f"shape of valid Y = {label_valid_folds.shape}")
     # clf = svm.SVC(kernel='linear')
     # TODO figure out which one is nice 
-    clf = svm.SVC(kernel='linear', C=0.1)
+    # clf = svm.SVC(kernel='linear', C=0.1)
+    # clf.fit(x_train, y_train)svm = LinearSVC()
+
+    svm_ = svm.LinearSVC(dual=False, fit_intercept=False, verbose=0, class_weight='balanced')
+    clf = CalibratedClassifierCV(svm_) 
     clf.fit(x_train, y_train)
-    # clf = svm.LinearSVC(dual=False, fit_intercept=False, verbose=0, class_weight='balanced')
     # param_grid = {'C': [0.0001, 0.001, 0.01, 0.1, 1.]}
     # grds = GridSearchCV(clf, param_grid, n_jobs=4, verbose=0)
     # grds.fit(x_train, y_train)
+    
+    # clf = LogisticRegression(C=0.1, class_weight='balanced', dual=False, fit_intercept=False,
+    #     intercept_scaling=1, max_iter=10000, multi_class='ovr', n_jobs=1,
+    #     penalty='l2', random_state=None, solver='liblinear', tol=0.001,
+    #     verbose=0, warm_start=False)
+    # clf.fit(x_train, y_train)
 
-    predicted_labels = clf.predict(feature_valid_folds)
+    predicted_probabilities = clf.predict_proba(feature_valid_folds)
+    # label = 1 if probability of 1 > 0.5 else 0
+    predicted_labels = predicted_probabilities[:,1] > 0.5
     n_errors = np.sum(abs(predicted_labels-label_valid_folds))
     print(n_errors/len(label_valid_folds))
     bar()
