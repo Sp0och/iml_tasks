5a99b3aeba5e2600b8bdb5cdf79b2bc5973f90a1
afuhrer <afuhrer@student.ethz.ch>
2022-04-21 10:04:40 +0200

    task2_3

--- task2_3.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn import svm
-from sklearn import metrics
-import matplotlib.pyplot as plt
-import pandas as pd
-from alive_progress import alive_bar
-from sklearn.model_selection import GridSearchCV
