1a3cf0df7a13ed933c0c9a15403e91dfd4702a79
Spooch <fierz.lasse@gmail.com>
2022-05-01 00:08:09 +0200

    basic class structure of model manager with tests

--- task3/model.py
+++ task3/model.py
@@ -1,6 +1,8 @@
 from xml.parsers.expat import model
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 #choose minimum and resize accordingly before inputting
 IMG_HEIGHT = 256
@@ -33,3 +35,101 @@ def create_model(freeze=True):
     model = tf.keras.Model(inputs=inputs, outputs=embeddings)
     model.summary()
     return model
+
+
+class model_manager:
+    def __init__(self,img_height,img_width):
+        self.IMG_HEIGHT = img_height
+        self.IMG_WIDTH = img_width
+        #create data augmentation by randomly switching horizontally and randomly rotate by 0.1
+        self.inputs = tf.keras.Input(shape=(3, self.IMG_HEIGHT, self.IMG_WIDTH, 3))
+        data_augmentation = tf.keras.Sequential(
+            [
+                tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+                tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+            ]
+        )
+        norm_layer = tf.keras.layer.experimental.preprocessing.Normalization()
+        mean = np.array([127.5]*3)
+        var = mean**2
+        encoder =  tf.keras.applications.EfficientNetB4(
+        include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3), weights="imagenet")
+        encoder.trainable = False
+        decoder = tf.keras.Sequential([
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(128),
+            tf.keras.layers.Lambda(
+                lambda t: tf.math.l2_normalize(t, axis=1))
+        ])
+        image, similar, different = self.inputs[:, 0, ...], self.inputs[:, 1, ...], self.inputs[:, 2, ...]
+        image_normed = norm_layer(data_augmentation(image))
+        similar_normed = norm_layer(data_augmentation(similar))
+        different_normed = norm_layer(data_augmentation(different))
+        norm_layer.set_weights([mean,var])
+        image_features = decoder(encoder(image_normed))
+        similar_features = decoder(encoder(similar_normed))
+        different_features = decoder(encoder(different_normed))
+        self.outputs = tf.stack([image_features,similar_features,different_features],axis=-1)
+        self.model = tf.keras.Model(inputs=self.inputs,outputs=self.outputs)
+        #create the basic model with the augmentation, encoder and decoder layers
+    def loss_function(self):
+        print("hello world")
+    def metric(self):
+        print("hello world")
+    def set_optimizer(self):
+        print("hello world")
+    def fit(self):
+        print("hello world")
+    def add_predictor(self):
+        print("hello world")
+
+#preview of data augmentation
+# image = plt.imread("00013.jpg")
+# plt.figure(figsize=(10,10))
+# data_augmentation = tf.keras.Sequential(
+#     [
+#         tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+#         tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+#     ]
+# )
+# for i in range(9):
+#         ax = plt.subplot(3, 3, i + 1)
+#         augmented_image = data_augmentation(
+#             tf.expand_dims(image, 0), training=True
+#         )
+#         plt.imshow(augmented_image[0].numpy().astype("int32"))
+#         plt.axis("off")
+# plt.show()
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
+#create data augmentation by randomly switching horizontally and randomly rotate by 0.1
+inputs = tf.keras.Input(shape=(3, IMG_HEIGHT, IMG_WIDTH, 3))
+data_augmentation = tf.keras.Sequential(
+    [
+        tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
+norm_layer = tf.keras.layers.experimental.preprocessing.Normalization()
+mean = np.array([127.5]*3)
+var = mean**2
+encoder =  tf.keras.applications.EfficientNetB4(
+include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3), weights="imagenet")
+encoder.trainable = False
+decoder = tf.keras.Sequential([
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(128),
+    tf.keras.layers.Lambda(
+        lambda t: tf.math.l2_normalize(t, axis=1))
+])
+image, similar, different = inputs[:, 0, ...], inputs[:, 1, ...], inputs[:, 2, ...]
+image_normed = norm_layer(data_augmentation(image))
+similar_normed = norm_layer(data_augmentation(similar))
+different_normed = norm_layer(data_augmentation(different))
+# norm_layer.set_weights([mean,var])
+image_features = decoder(encoder(image_normed))
+similar_features = decoder(encoder(similar_normed))
+different_features = decoder(encoder(different_normed))
+outputs = tf.stack([image_features,similar_features,different_features],axis=-1)
+model = tf.keras.Model(inputs=inputs,outputs=outputs)
+model.summary()
