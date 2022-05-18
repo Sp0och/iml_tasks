e62c03bf785012b4da5ce007db949299ad0b6958
sp0och <fierz.lasse@gmail.com>
2022-05-18 23:15:28 +0200

    removed auto index numbers in processed datasets

--- task4/build_HE_nn.py
+++ task4/build_HE_nn.py
@@ -1,14 +1,15 @@
-
-# Build and fit a NN regarding the reduced pretrain data and the respective labels
+import torch
+import numpy as np
+import pandas as pd
+# TODO Build and fit a NN regarding the reduced pretrain data and the respective labels
 
 #Load in dataset
 
-#Create NN structure
+#Create NN structure – outside class definition
 
 #Define Loss function and compilation as a whole
 
 #Fit the NN
 
 #Save the model
-
 
